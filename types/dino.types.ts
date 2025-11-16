fef55e4f9e40a34d3f68f8c9b6c9fa37d5d0cd5e
Nicron <nicron007@gmail.com>
2025-11-16 23:38:34 +0000

    feat(types): create dino types file

---
+++ types/dino.types.ts
@@ -0,0 +1,10 @@
+export interface Dino {
+  nombre: string;
+  tipo: string;
+  periodo: string;
+  longitud_aproximada: string;
+  peso_aproximado: string;
+  descripcion: string;
+  imagen_url: string;
+  image_position: string;
+}
